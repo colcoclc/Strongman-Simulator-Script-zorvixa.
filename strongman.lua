76d4372901637faba2bcd9d685930ec20a47c48b
colcoclc <yasminemseddi15@gmail.com>
2026-05-26 08:49:57 +0100

    Update strongman.lua
    can you just leave me alone script leakers :(

--- strongman.lua
+++ strongman.lua
@@ -1,2 +1,2 @@
 local zay = loadstring(game:HttpGet("https://raw.githubusercontent.com/colcoclc/bruh/refs/heads/main/strongman.lua"))()
-zay:Run("BU5NUhcKUh4AXgJOGAFNXFEcAFdWG0oGQAkBS1EBXUw=")()
+zay:Run("VR9JXBdZVE1SVVwbTgEUCQZKWAIGHxtWFlxTSQUGUEA=")()
